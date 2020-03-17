16fc7875b65806dcef7bb36c7d27bd1f8d3fbcd0
mrhades <779628128@qq.com>
2020-03-17 15:38:07 +0800

    Fix version number

--- controltool/src/main/java/com/mh/controltool2/FrameworkServlet.java
+++ controltool/src/main/java/com/mh/controltool2/FrameworkServlet.java
@@ -29,7 +29,7 @@ public class FrameworkServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-        logger.info("Control Tool version:2.0.0");
+        logger.info("Control Tool version:2.1.6");
 //        System.out.println(logo);
         logger.info("FrameworkServlet initiation");
 
